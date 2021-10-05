94c777b71f1cc67d5b842f8950fbc66b9586cc0e
Kolya Venturi <kolya.venturi@enx3s.com>
2021-10-04 18:12:54 -0700

    Fix precommit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {AppProps} from 'next/dist/shared/lib/router/router';
-import '../styles/index.scss';
 
 const App = ({Component, pageProps}: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
