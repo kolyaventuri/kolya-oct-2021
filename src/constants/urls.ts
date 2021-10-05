330ee6d0535049bdd8582c652684f496ebddb6d2
Kolya Venturi <kolya.venturi@enx3s.com>
2021-10-05 13:45:29 -0700

    Allow for a local test WS server

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,1 +1,3 @@
-export const BOOK_URL = 'wss://www.cryptofacilities.com/ws/v1';
+/* eslint-disable node/prefer-global/process */
+export const BOOK_URL =
+  process.env.NEXT_PUBLIC_WS_HOST ?? 'wss://www.cryptofacilities.com/ws/v1';
