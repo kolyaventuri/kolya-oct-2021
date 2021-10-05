6e65fa5ec8f38900ec01a9e6ca4b9734925bb6c7
Kolya Venturi <kolya.venturi@enx3s.com>
2021-10-04 18:15:21 -0700

    Init tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {AppProps} from 'next/dist/shared/lib/router/router';
+import 'tailwindcss/tailwind.css';
 
 const App = ({Component, pageProps}: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
