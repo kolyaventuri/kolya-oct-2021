175d74398d69bcd068736aa239c6a0a97ab88d93
Kolya Venturi <kolya.venturi@enx3s.com>
2021-10-05 15:06:06 -0700

    Setup Local Feed (#3)
    * Add dummy data
    
    * Allow for a local test WS server
    
    * Allow for a local test WS server
    
    * Basic readme
    
    * Update readme

--- README.md
+++ README.md
@@ -1,32 +1,28 @@
-# Amplify + Typescript Template
-_Template repository for AWS Amplify apps using Typescript and NextJS_
+# CryptoFacilities Order Book Example
+_Example WebSocket app, reading off the CryptoFacilities WS API_
 
 [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/xojs/xo)
 ---
 
 # Overview
-Run fast with a new [AWS Amplify](https://aws.amazon.com/amplify/) project using a quick and dirty TypeScript stack! Really built for single page web apps, but do with this what you want.
+Renders an order book for XBTUSD or ETHUSD, using the CryptoFacilities WebSocket API.
 
 # Stack
 - Frontend
   - [NextJS](https://nextjs.org/)
   - [React](https://reactjs.org)
-  - [SASS](https://sass-lang.com/)
   - [TypeScript](https://www.typescriptlang.org/)
+  - [TailwindCSS](https://tailwindcss.com/)
 - Testing
   - [AVA](https://github.com/avajs/ava) (test runner)
   - [XO](https://github.com/xojs/xo) (code linting) 
-  - [stylelint](https://stylelint.io/) (SASS linting)
 
-# How to use
-1) Create a new repository using this as a template
-2) `npm i` to get the dependencies installed
-3) Modify the `package.json` and this readme to fit your project
-4) Run the local build with `npm run dev`! (App will start on port `4000`)
-5) Deploy your app on Amplify! See their [docs](https://docs.amplify.aws/) for more information on your specific deployment strategy.
+# Setup + Running
+- Clone the repo and `npm i` to install dependencies
+- Run with one of the following
+  - Use `npm run dev` to run locally, with a live connection to CryptoFacilities
+  - Use `npm run local` to run locally, with a **mocked** local websocket
+- App will run on `http://localhost:4000`
 
-# Q + A
-### Why AVA? Why not Jest?
-- Personal preference. I find Jest clunky in comparison, but it has its merits. Feel free to swap it out.
-### Why use such a strict linter?
-- I like clean code, and personally it forces me to write more readable code. The style is automagically enforced by Prettier anyways.
+# Deployment
+Steps TBD
---
+++ server/socket.ts
@@ -0,0 +1,92 @@
+/*
+ * (kolyaventuri): Extremely rudimentary websocket server, designed
+ * to mimic the CryptoFacilities WS endpoint, for testing purposes only.
+ *
+ * Not to be used for true-to-life data, as it's just going to spit back
+ * the same data in a loop.
+ */
+
+/* TODO: Eslint (via XO) is disabled for this file due to seemingly un-resolvable indentation issues.
+ * Prettier keeps fighting the inter and I need to look into why. */
+
+import {WebSocket, WebSocketServer} from 'ws';
+
+import xbtInit from '../fixtures/pi_xbtusd.init.json';
+import xbtRunning from '../fixtures/pi_xbtusd.json';
+
+const dataSets: Record<
+  string,
+  {
+    init: typeof xbtInit;
+    running: typeof xbtRunning;
+  }
+> = {
+  PI_XBTUSD: {
+    init: xbtInit,
+    running: xbtRunning,
+  },
+};
+
+const wss = new WebSocketServer({port: 8080});
+
+const connections: Record<
+  string,
+  {
+    socket: WebSocket;
+    subscriptions: string[];
+    next: number;
+  }
+> = {};
+
+wss.on('connection', (ws) => {
+  const id = Math.random().toString();
+  connections[id] = {
+    socket: ws,
+    subscriptions: [],
+    next: 0,
+  };
+  console.log('WS Connection Established');
+
+  ws.on('message', (message) => {
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string
+    const data = JSON.parse(message.toString()) as Record<string, any>;
+
+    if (data.event === 'subscribe') {
+      // (kolyaventuri): Send initial data
+      for (const pid of data.product_ids as string[]) {
+        for (const data of dataSets[pid].init) {
+          connections[id].socket.send(JSON.stringify(data));
+        }
+      }
+
+      connections[id].subscriptions.push(...(data.product_ids as string[]));
+    } else if (data.event === 'unsubscribe') {
+      for (const key of data.product_ids as string[]) {
+        const i = connections[id].subscriptions.indexOf(key);
+        if (i > -1) {
+          connections[id].subscriptions.splice(i, 1);
+        }
+      }
+    }
+  });
+});
+
+setInterval(() => {
+  for (const conn of Object.values(connections)) {
+    for (const sub of conn.subscriptions) {
+      if (dataSets[sub]) {
+        const index = conn.next;
+
+        conn.socket.send(JSON.stringify(dataSets[sub].running[index]));
+        conn.next += 1;
+        if (conn.next >= dataSets[sub].running.length) {
+          conn.next = 0;
+        }
+      }
+    }
+  }
+}, 100);
+
+wss.on('listening', () => {
+  console.log('Local socket server listening on 8080');
+});
