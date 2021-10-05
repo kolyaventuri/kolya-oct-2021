c9b18792853c6b715624b8cb39728baa6780035b
Kolya Venturi <kolya.venturi@enx3s.com>
2021-10-05 13:21:09 -0700

    Allow socket to be opened and closed

--- src/util/socket.ts
+++ src/util/socket.ts
@@ -2,19 +2,33 @@ import {BOOK_URL} from '../constants/urls';
 import {SubscribeOptions, UnsubscribeOptions} from '../types/socket';
 
 type CallbackFn<T> = (event: T) => unknown;
+const events = ['open', 'error', 'close', 'message'] as const;
+type EventType = typeof events[number];
+
+const enum ReadyState {
+  CONNECTING,
+  OPEN,
+  CLOSING,
+  CLOSED,
+}
 
 export class WrappedSocket {
-  private readonly __socket: WebSocket;
+  private __socket: WebSocket;
+  private readonly __url: string;
+  private readonly __handlers: {[key in EventType]?: CallbackFn<any>} = {};
 
   constructor(url: string) {
+    this.__url = url;
     this.__socket = new window.WebSocket(url);
+    this.open();
   }
 
   public on(event: 'open' | 'error', callback: CallbackFn<Event>): void;
   public on(event: 'close', callback: CallbackFn<CloseEvent>): void;
   public on(event: 'message', callback: CallbackFn<MessageEvent>): void;
-  public on(event: string, callback: CallbackFn<any>): void {
+  public on(event: EventType, callback: CallbackFn<any>): void {
     const handler = `on${event}`;
+    this.__handlers[event] = callback;
     this.__socket[handler] = callback;
   }
 
@@ -26,6 +40,23 @@ export class WrappedSocket {
       ...options,
     });
     this.__socket.send(eventString);
+  }
+
+  public close(): void {
+    this.__socket.close();
+  }
+
+  public open(): void {
+    if (this.__socket.readyState !== ReadyState.CLOSED) {
+      return;
+    }
+
+    this.__socket = new window.WebSocket(this.__url);
+    for (const [event, callback] of Object.entries(this.__handlers)) {
+      // @ts-expect-error - Event can only be one of EventType
+      // TODO: Resolve this type if possible
+      this.on(event, callback);
+    }
   }
 }
 
--- test/util/socket.test.ts
+++ test/util/socket.test.ts
@@ -1,8 +1,19 @@
 import test from 'ava';
 import {stub} from 'sinon';
+import proxyquire from 'proxyquire';
+import {
+  getSocket as realGetSocket,
+  WrappedSocket as RealWrappedSocket,
+} from '../../src/util/socket';
+import {delay} from '../helpers/delay';
 
-import {BOOK_URL} from '../../src/constants/urls';
-import {getSocket, WrappedSocket} from '../../src/util/socket';
+const BOOK_URL = 'wss://some-url/';
+const {getSocket, WrappedSocket} = proxyquire<{
+  getSocket: typeof realGetSocket;
+  WrappedSocket: typeof RealWrappedSocket;
+}>('../../src/util/socket', {
+  '../constants/urls': {BOOK_URL},
+});
 
 test('#getSocket returns a memoized wrapped socket', (t) => {
   const result = getSocket();
@@ -70,4 +81,73 @@ test('WrappedSocket#send sends an event to the socket', (t) => {
   );
 
   sendStub.restore();
+});
+
+test('WrappedSocket#close closes the socket', (t) => {
+  const closeStub = stub(window.WebSocket.prototype, 'close');
+  const socket = getSocket();
+
+  socket.close();
+  t.true(closeStub.called);
+
+  closeStub.restore();
+});
+
+test('WrappedSocket#open reopens a closed socket', async (t) => {
+  const socket = getSocket();
+  const OldWS = window.WebSocket;
+  const constructorStub = stub().callsFake(
+    (url: string, ...args: any[]) => new OldWS(url, ...args),
+  );
+
+  // @ts-expect-error - Overwrite
+  window.WebSocket = (...args: any[]) => {
+    return constructorStub(...args);
+  };
+
+  socket.close();
+  await delay();
+  socket.open();
+
+  t.true(constructorStub.called);
+});
+
+test('WrappedSocket#open re-binds whichever events were listened', async (t) => {
+  const socket = getSocket();
+  const fn = () => {};
+  socket.on('open', fn);
+
+  const OldWS = window.WebSocket;
+  const constructorStub = stub().callsFake(
+    (url: string, ...args: any[]) => new OldWS(url, ...args),
+  );
+
+  // @ts-expect-error - Overwrite
+  window.WebSocket = (...args: any[]) => {
+    return constructorStub(...args);
+  };
+
+  socket.close();
+  await delay();
+  socket.open();
+
+  // @ts-expect-error - Bypass private to check for existence
+  t.is(socket.__socket.onopen, fn);
+});
+
+test('WrappedSocket#open, if socket is already open, does not re-open it', (t) => {
+  const socket = getSocket();
+  const OldWS = window.WebSocket;
+  const constructorStub = stub().callsFake(
+    (url: string, ...args: any[]) => new OldWS(url, ...args),
+  );
+
+  // @ts-expect-error - Overwrite
+  window.WebSocket = (...args: any[]) => {
+    return constructorStub(...args);
+  };
+
+  socket.open();
+
+  t.false(constructorStub.called);
 });
