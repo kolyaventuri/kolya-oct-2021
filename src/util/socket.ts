43596184fc4f05466af606d131e0763441f2cba6
Kolya Venturi <kolya.venturi@enx3s.com>
2021-10-05 09:53:06 -0700

    Add event handlers, and sub/unsub to wrapped socket

--- src/util/socket.ts
+++ src/util/socket.ts
@@ -1,11 +1,31 @@
 import {BOOK_URL} from '../constants/urls';
+import {SubscribeOptions, UnsubscribeOptions} from '../types/socket';
+
+type CallbackFn<T> = (event: T) => unknown;
 
 export class WrappedSocket {
   private readonly __socket: WebSocket;
 
   constructor(url: string) {
     this.__socket = new window.WebSocket(url);
-    console.log(typeof this.__socket);
+  }
+
+  public on(event: 'open' | 'error', callback: CallbackFn<Event>): void;
+  public on(event: 'close', callback: CallbackFn<CloseEvent>): void;
+  public on(event: 'message', callback: CallbackFn<MessageEvent>): void;
+  public on(event: string, callback: CallbackFn<any>): void {
+    const handler = `on${event}`;
+    this.__socket[handler] = callback;
+  }
+
+  public send(event: 'subscribe', options: SubscribeOptions): void;
+  public send(event: 'unsubscribe', options: UnsubscribeOptions): void;
+  public send(event: string, options: Record<string, any>): void {
+    const eventString = JSON.stringify({
+      event,
+      ...options,
+    });
+    this.__socket.send(eventString);
   }
 }
 
--- test/util/socket.test.ts
+++ test/util/socket.test.ts
@@ -1,4 +1,5 @@
 import test from 'ava';
+import {stub} from 'sinon';
 
 import {BOOK_URL} from '../../src/constants/urls';
 import {getSocket, WrappedSocket} from '../../src/util/socket';
@@ -11,4 +12,62 @@ test('#getSocket returns a memoized wrapped socket', (t) => {
 
   const result2 = getSocket();
   t.is(result2, result);
+});
+
+test('can add open event to wrapped socket', (t) => {
+  const socket = getSocket();
+  const fn = () => {};
+  socket.on('open', fn);
+
+  // @ts-expect-error - Bypass private to check for existence
+  t.is(socket.__socket.onopen, fn);
+});
+
+test('can add close event to wrapped socket', (t) => {
+  const socket = getSocket();
+  const fn = () => {};
+  socket.on('close', fn);
+
+  // @ts-expect-error - Bypass private to check for existence
+  t.is(socket.__socket.onclose, fn);
+});
+
+test('can add error event to wrapped socket', (t) => {
+  const socket = getSocket();
+  const fn = () => {};
+  socket.on('error', fn);
+
+  // @ts-expect-error - Bypass private to check for existence
+  t.is(socket.__socket.onerror, fn);
+});
+
+test('can add message event to wrapped socket', (t) => {
+  const socket = getSocket();
+  const fn = () => {};
+  socket.on('message', fn);
+
+  // @ts-expect-error - Bypass private to check for existence
+  t.is(socket.__socket.onmessage, fn);
+});
+
+test('WrappedSocket#send sends an event to the socket', (t) => {
+  const sendStub = stub(window.WebSocket.prototype, 'send');
+  const socket = getSocket();
+
+  socket.send('subscribe', {
+    feed: 'abc',
+    product_ids: ['123'],
+  });
+
+  t.true(
+    sendStub.calledWith(
+      JSON.stringify({
+        event: 'subscribe',
+        feed: 'abc',
+        product_ids: ['123'],
+      }),
+    ),
+  );
+
+  sendStub.restore();
 });
