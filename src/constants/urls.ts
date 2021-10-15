2ebd8d731347317050878f001d6b8ac8fe54e3f6
Kolya Venturi <me@kolya.co>
2021-10-15 14:45:29 -0700

    Fix issue with live data

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,3 +1,4 @@
 /* eslint-disable node/prefer-global/process */
-export const BOOK_URL =
-  process.env.NEXT_PUBLIC_WS_HOST ?? 'wss://www.cryptofacilities.com/ws/v1';
+const prodUrl = 'wss://www.cryptofacilities.com/ws/v1';
+const url = process.env.NEXT_PUBLIC_WS_HOST ?? prodUrl;
+export const BOOK_URL = process.env.NEXT_PUBLIC_ENV === 'local' ? url : prodUrl;
