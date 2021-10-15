321b20b771f72ae3730ea4d9725d8e8b5a11068e
Kolya Venturi <kolya.venturi@enx3s.com>
2021-10-14 18:42:12 -0700

    WIP Changes

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -5,26 +5,23 @@ import userEvent from '@testing-library/user-event';
 
 import Home from '../../src/pages';
 
-test.afterEach(cleanup);
+test.before(() => {
+  render(<Home/>);
+});
+test.after(cleanup);
 
 test('renders an order book', (t) => {
-  // Ava, being async, doesn't play nice with the global-DOM that react testing lib uses
-  const {container} = render(<Home />);
-
-  t.not(container.querySelector('[data-testid="order-book"]'), undefined);
+  t.not(screen.getByTestId('order-book'), undefined);
 });
 
 test('renders a header with the right ticker', (t) => {
-  const {container} = render(<Home />);
-
-  const header = container.querySelector('[data-testid="header"]');
+  const header = screen.getByTestId('header');
 
   t.not(header, undefined);
   t.true(header?.textContent?.includes('XBTUSD'));
 });
 
 test('renders a toggle button in the order book, that when clicked, toggles the ticker', t => {
-  render(<Home />);
   const button = screen.getByText('Toggle Feed');
 
   t.true(screen.getByTestId('header').innerText.includes('XBTUSD'));
